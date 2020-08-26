555c29cb3754c889ebf2884f084c1e25b1eb74b7
CalmProton <39672167+CalmProton@users.noreply.github.com>
2020-08-26 22:13:29 +0300

    Update index.js

--- DiscordBot/index.js
+++ DiscordBot/index.js
@@ -3,4 +3,14 @@ const bot = new Discord.Client();
 
 const token = 'NzQ4MjUzMzI2OTgxODU3MzM3.X0avNw.v3XsOA5x07IJ47eDOoPJUhVOdjQ';
 
+bot.on('ready', () => {
+    console.log('Bot is Online!');
+})
+
+bot.on('message', msg=>{
+    if(msg.content ===  "HELLO"){
+        msg.reply('WORLD');
+    }
+})
+
 bot.login(token);
