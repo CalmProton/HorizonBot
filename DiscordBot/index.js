aba1fd331ed80b6c53701d9c9543d0c6d89004a8
CalmProton <39672167+CalmProton@users.noreply.github.com>
2020-08-26 22:42:27 +0300

    Update index.js

--- DiscordBot/index.js
+++ DiscordBot/index.js
@@ -3,13 +3,21 @@ const bot = new Discord.Client();
 
 const token = 'NzQ4MjUzMzI2OTgxODU3MzM3.X0avNw.v3XsOA5x07IJ47eDOoPJUhVOdjQ';
 
+const PREFIX = '%';
+
 bot.on('ready', () => {
     console.log('Bot is Online!');
 })
 
-bot.on('message', msg=>{
-    if(msg.content ===  "HELLO"){
-        msg.reply('WORLD');
+bot.on('message', message=>{
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'ping':
+            message.channel.send('pong!')
+            break;
+
     }
 })
 
