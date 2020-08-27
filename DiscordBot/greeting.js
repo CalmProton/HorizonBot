d929303c39b4d442f31d4530b8cbbf64929c18db
CalmProton <39672167+CalmProton@users.noreply.github.com>
2020-08-27 13:30:00 +0300

    fixed memberjoin event

--- DiscordBot/greeting.js
+++ DiscordBot/greeting.js
@@ -1,9 +1,8 @@
 const {Client, RichEmbed} = require('discord.js');
 const bot = new Client();
 
-bot.on('guildMemberAdd', member =>){
+bot.on('guildMemberAdd', member =>{
     const welcomeEmbed = new RichEmbed()
     .setTitle ("Welcome Embed");
-    
     message.author.send(Embed);
-}
+})
