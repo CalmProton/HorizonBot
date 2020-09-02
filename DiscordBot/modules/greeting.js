e1eda79b38a596626c568b413c51348dfeb9e36d
CalmProton <39672167+CalmProton@users.noreply.github.com>
2020-09-02 13:26:27 +0300

    updated greeting embed

--- DiscordBot/modules/greeting.js
+++ DiscordBot/modules/greeting.js
@@ -5,8 +5,12 @@ module.exports = bot => {
 
 bot.on('guildMemberAdd', member =>{
     const welcomeEmbed = new Discord.MessageEmbed()
-    .setTitle ("Welcome Embed");
-
+    .setTitle ("Добро пожаловать")
+    .setURL('https://vk.com/horizoncraft/')
+    .setAuthor('HorizonCraft')
+    .setDescription('Проблем не обнаружено')
+    .addField('IP адрес: play.horizoncraft.ru')
+    .setFooter('Группа ВК: vk.com/horizoncraft')
     member.send(welcomeEmbed);
 })
 
